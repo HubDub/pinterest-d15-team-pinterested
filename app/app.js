26ae00e7cd3b2ecd91d91b8bd3d4e446e07e8115
Elizabeth Sanger <libsang@gmail.com>
2016-09-08 16:52:19 -0500

    added pathing, controllers, etc.

--- app/app.js
+++ app/app.js
@@ -3,6 +3,58 @@ var app = angular.module("PinterestApp", ["ngRoute"])
 .constant('FirebaseURL','https://pinterest-75ff6.firebaseio.com/');
 
 
+app.config(function($routeProvider){
+
+    let isAuth = (AuthFactory) => new Promise( (resolve, reject) =>
+     {
+        if(AuthFactory.isAuthenticated()) {
+            resolve();
+        } else {
+            reject();
+        }
+    });
+
+    $routeProvider.
+        when("/", {
+            templateUrl: "partials/login.html",
+            controller: "LoginCtrl"
+        }).
+
+        when("/login", {
+            templateUrl:"partials/login.html",
+            controller: "LoginCtrl"
+        }).
+        when("/allboards", {
+            templateUrl: 'partials/allBoards.html',
+            controller: "AllBoardsCtrl",
+            resolve: {isAuth}
+
+        }).
+        when("/allpins", {
+            templateUrl: 'partials/pinListView.html',
+            controller: "pinListViewCtrl",
+            resolve: {isAuth}
+        }).
+        when('/:boardId', {
+
+            templateUrl: "partials/oneBoard.html",
+            controller: "OneBoardCtrl",
+            resolve: {isAuth}
+        }).
+
+        when("/:pinId", {
+            templateUrl: "partials/savePin.html",
+            controller: "SavePinCtrl",
+            resolve: {isAuth}
+        }).
+
+        otherwise("/");
+
+});
+
+
+
+
 app.run( ($location, FBCreds) => {
     let creds = FBCreds;
     let authConfig = {
