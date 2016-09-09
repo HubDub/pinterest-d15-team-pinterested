436b6119bba69573a7d7e7066afb00749e0c206a
Elizabeth Sanger <libsang@gmail.com>
2016-09-09 07:12:51 -0500

    login/logout fully functional

--- app/controllers/NavCtrl.js
+++ app/controllers/NavCtrl.js
@@ -1,7 +1,9 @@
 "use strict";
 
-app.controller("NavCtrl", function($scope, searchTermData, $location) {
-    $scope.searchText = searchTermData;
+app.controller("NavCtrl", function($scope,
+    // searchTermData,
+    $location) {
+    // $scope.searchText = searchTermData;
     $scope.navItems = [    //create an array of objects
             {url: "#/logout", name: "Logout", showState: "$parent.isLoggedIn"},
             {url: '#/login', name: "Login", showState: "!$parent.isLoggedIn"},
