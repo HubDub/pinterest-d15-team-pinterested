877dd3f9a576506585b5cf14b9f7525c9a3cf279
Elizabeth Sanger <libsang@gmail.com>
2016-09-11 15:53:03 -0500

    was making it way too complicated. Lots of extra edit buttons attached to each pic, but allBoards is filtering correct pins to correct boards

--- app/controllers/AllBoardsCtrl.js
+++ app/controllers/AllBoardsCtrl.js
@@ -1,38 +1,29 @@
 "use strict";
 
 app.controller("AllBoardsCtrl", function ($scope, PinFactory, $routeParams) {
-  let boards = [];
+  // let boards = [];
   let user = $scope.$parent.getUser();
-  let boardId;
+  let boardId = [];
   let pins = [];
-  let boardIdsArr = [];
 
-//The function below gets the user boards from FB; then, before it is finished, it gets userPins based on the board ID.
+
   PinFactory.getUserBoards(user)
     .then( (boards) => {
-      console.log("we are in getAllBoards these are boards", boards);
-      // for (var board in boardObj) {
-      //   boardObj[board].id = board;
-      //   userBoards.push(boardObj[board]);
-      // }
-      // console.log("you have boards", boards);
       $scope.boards = boards;
-      boards.forEach(function(board){
-        // console.log(board.id)
-        boardId = board.id
-      console.log(boards)
+      console.log('these are the boards', $scope.boards)
     })
 
-   //the problem is right below: even though a loop is running based on the length of the boards array obtained above, getUserPins is only being called on one of the boards in the array, not all of them.
+  // PinFactory.getUserPins(board.id)
+  // .then((result) => {
+  // console.log('result of get user pins', result)
+  // $scope.pins = result;
+  //     })
 
-    for (var i = 0; i < boards.length; i++) {
-    PinFactory.getUserPins(boardId)
-    .then((result) => {
-      console.log('result of get user pins', result)
-      $scope.pins = result;
-      })
-      }
-    });
+  PinFactory.getAllPins()
+  .then((pins) => {
+    $scope.pins = pins;
+  })
+
+   });
 
 
-});
