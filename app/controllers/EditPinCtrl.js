7fd8b4d279dbd7b2d7ab0de4ccafccc3ca78cf2a
Meg Ducharme <ducharme.meg@gmail.com>
2016-09-12 12:24:27 -0500

    Fixed few styling issues

--- app/controllers/EditPinCtrl.js
+++ app/controllers/EditPinCtrl.js
@@ -17,7 +17,7 @@ $scope.addNewPin = function () {
   console.log("add new pin is running");
     PinFactory.updatePin($routeParams.pinId, $scope.newPin)
     .then(function(){
-        $location.url('#/allBoard');
+        $location.url('/allboards');
     });
   };
 });
